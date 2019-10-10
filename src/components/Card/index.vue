ced05769f88dfe1f1d44158a4a03377bf6223c4a
RaphaelRT <raphael.robert@hetic.net>
2019-10-10 19:41:57 +0200

    card component

---
+++ src/components/Card/index.vue
@@ -0,0 +1,24 @@
+<template>
+     <div class="card">
+       <div class="img">
+           <img v-bind:src="image" alt="">
+       </div>
+       <div class="content">
+           <p>{{text}}</p>
+       </div>
+     </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props:  {
+    text: String,
+    image: String
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    @import "style.scss";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card text="Bonjour mes couilles sur un tatamie ça fait un radeau"
+          image="img/icons/android-chrome-192x192.png"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card/'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
